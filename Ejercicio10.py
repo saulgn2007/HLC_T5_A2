25e6032c43dfefc227f0c1921d6bcf56bfd6ebbc
saulgn2007 <saulgonzaleznavarro2007@gmail.com>
2025-01-24 12:45:53 +0100

    Update Ejercicio10.py

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -1,7 +1,3 @@
-def dibuja ():
-    num_estrellas = int(input("Introduce el número de estrellas: "))
-    contador = 1
-    estrellas = "★"
-    for index in range(num_estrellas):
-        print(estrellas + contador)
-        contador += 1
+num = int(input("Ingresa un número: "))
+for i in range(1, num + 1):
+    print("★" * i)
